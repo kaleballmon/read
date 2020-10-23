16dd100eabc2890cce29ead4bd54d9774e837e17
Kaleb Allmon <kdall09@gmail.com>
2020-10-23 11:32:25 -0400

    added readings.css

--- src/pages/readings.js
+++ src/pages/readings.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
 import Dropdown from "../components/dropdown"
+import "../styles/readings.css"
 
 const Readings = () => {
   return (
     <Layout>
-      <Dropdown />
+      <div></div>
     </Layout>
   )
 }
