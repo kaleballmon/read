54f7f09da5025b6b7ffe994d674663baeb9f1ec5
Kaleb Allmon <kdall09@gmail.com>
2020-10-23 12:06:10 -0400

    finished reading page template and basic styling

---
+++ src/components/sidebar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import sidebarStyles from "../style-modules/sidebar.module.css"
+
+const Sidebar = () => {
+  return (
+    <div className={sidebarStyles.container}>
+      <div>There will be sidebar things here.</div>
+    </div>
+  )
+}
+
+export default Sidebar
--- src/pages/readings.js
+++ src/pages/readings.js
@@ -1,12 +1,18 @@
 import React from "react"
 import Layout from "../components/layout"
 import Dropdown from "../components/dropdown"
+import Sidebar from "../components/sidebar"
 import "../styles/readings.css"
 
 const Readings = () => {
   return (
     <Layout>
-      <div></div>
+      <div id="readingContainer">
+        <Sidebar />
+        <div id="catalog">
+          <Dropdown />
+        </div>
+      </div>
     </Layout>
   )
 }
