b191c8143003c1d7965a00128b7b448df7043c91
Kaleb Allmon <kdall09@gmail.com>
2020-10-23 10:29:09 -0400

    last commit didn't track the new about page files. this one does

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/layout"
+
+import "../styles/about.css"
+
+const About = () => {
+  return (
+    <Layout>
+      <div class="centerStandalone">
+        <div id="aboutContainer">This will be some about information.</div>
+      </div>
+    </Layout>
+  )
+}
+
+export default About
