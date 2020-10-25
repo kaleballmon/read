bb6c91598be097ce52a0217e65bc8ea19aee389d
Kaleb Allmon <kdall09@gmail.com>
2020-10-25 19:00:11 -0400

    added environment variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
+require('dotenv').config({path: __dirname + '/.env'})
+
 module.exports = {
 	plugins: [
 		{
 			resolve: `gatsby-source-mongodb`,
 			options: {
-				connectionString: `mongodb+srv://shell:ddcsg0HPzlVRf4mb@kluster.flfaf.mongodb.net`,
+				connectionString: `mongodb+srv://shell:${process.env.DB_KEY}@kluster.flfaf.mongodb.net`,
 				dbName: `read`,
 				collection: `authors`,
 			}
