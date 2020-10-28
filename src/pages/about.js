91f5cc458836ae68a79332c77e0110b33dbee370
Kaleb Allmon <kdall09@gmail.com>
2020-10-28 00:32:05 -0400

    added content and took away template stylings for reading and contact pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,16 @@ const About = () => {
   return (
     <Layout>
       <div class="centerStandalone">
-        <div id="aboutContainer">This will be some about information.</div>
+        <div id="aboutContainer">
+          My name is Kaleb. I'm a Computer Science Student at New York
+          University. I'm sharing things that I've read that are cool to me or
+          have informed who I am. There's not really a theme, and not much is
+          similar. For anyone looking for my projects, you can head to my{" "}
+          <a href="https://github.com/kaleballmon" target="_blank">
+            github
+          </a>
+          , where I keep all of that stuff.
+        </div>
       </div>
     </Layout>
   )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,13 @@ import "../styles/contact.css"
 const Contact = () => (
   <Layout>
     <div class="centerStandalone">
-      <div id="contactContainer">Contact Info will go here</div>
+      <div id="contactContainer">
+        I'm open to talk about readings or hear about some you have to share.
+        <br></br>
+        Reach me at kdall09@gmail.com
+        <br></br>
+        :)
+      </div>
     </div>
   </Layout>
 )
